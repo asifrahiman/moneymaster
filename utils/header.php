37143683781bedde959659d43ec9a5aa5d21b7e5
asifrahiman <rahiman.abdul.asif@gmail.com>
2019-11-09 23:41:47 +0530

    removed logo

--- utils/header.php
+++ utils/header.php
@@ -1,10 +1,7 @@
 <?php echo "	<div class = \"row\">
-		<div onclick=\"window.location='/';\" class = \"col-sm-1 col-md-2\">
-			<img src=\"utils/logo.jpg\" class=\"logo\" />
-		</div>
 		<div onclick=\"window.location='/';\" class = \"col-sm-2 col-md-2\">
 			<h2 class=\"title\">Moneymaster</h2>
 		</div>
-		<div class = \"col-sm-9 col-md-8\"></div>
+		<div class = \"col-sm-10 col-md-10\"></div>
 	</div>"
 ?>
